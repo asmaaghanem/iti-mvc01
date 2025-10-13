fa111a0629b565d6ec7d87eeb4494c7b80381e88
Asmaa Ghanem <asmaaghanem406@gmail.com>
2025-10-13 22:20:49 +0300

    Add project files.

---
+++ studentController/Context/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studentController.Models;
+
+namespace studentController.Context
+{
+    public class StudentContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=StudentDB;Trusted_Connection=True;Encrypt=false");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
+        
---
+++ studentController/Controllers/StudentController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using studentController.Context;
+
+namespace studentController.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentContext db = new StudentContext();
+
+        public IActionResult getAll()
+        {
+            var depts = db.Students.ToList();
+            return View("getAll",depts); //model
+        }
+
+        public IActionResult getById(int id)
+        {
+            var dept = db.Students.Find(id);
+            if (dept == null)
+                return NotFound();
+            return View("getById",dept); //model
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public ContentResult test()
+        //{
+        //    //ContentResult res = new ContentResult();
+        //    //res.Content = "hello from student";
+        //    return Content("hello from student");
+        //}
+        //public JsonResult testJson()
+        //{             //ContentResult res = new ContentResult();
+        //    //res.Content = "hello from student";
+        //    return Json(new { id = 1, name = "ahmed" });
+        //}
+
+        //public IActionResult testView()
+        //{
+        //    //ContentResult res = new ContentResult();
+        //    //res.Content = "hello from student";
+        //    return View(test);
+        //}
+    }
+}
