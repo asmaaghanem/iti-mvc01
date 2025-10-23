99f374c63967127d76cf101a3da129276848d2dd
asmaaghanem <asmaaghanem406@gmail.com>
2025-10-23 23:28:46 +0300

    Delete studentController directory

--- studentController/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace studentController.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
